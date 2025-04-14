c462536597a3387d9b4e174c210ec1b01e9df2fe
Mohd Azam <mdazamsaifee995@gmail.com>
2025-04-14 14:58:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -11,32 +10,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-import gdown
 
-# Define the path and Google Drive file ID
-file_path = "WELFake_Dataset.csv"
-file_id = "105Pg07PqFa0540QHi3aN_QkvAjk-3P3i"
-url = f"https://drive.google.com/uc?id={file_id}"
-
-# Download only if the file doesn't already exist
-if not os.path.exists(file_path):
-    st.info("Downloading dataset from Google Drive...")
-    gdown.download(url, file_path, quiet=False)
-
-# Read the CSV
-st.info("Loading dataset...")
-news_df = pd.read_csv(file_path)
-
-# Display columns for debug
-st.subheader("🧾 Column Names:")
-st.write(news_df.columns)
-
-st.subheader("🔍 Sample Data:")
-st.write(news_df.head())
-
-# Load Data
+# Load data
+news_df = pd.read_csv('WELFake_Dataset.csv')
 news_df = news_df.fillna(' ')
-news_df['content'] = news_df['title'] + " " + news_df['text']
+news_df['content'] = news_df['title'] + ' ' + news_df['text']
 X = news_df.drop('label', axis=1)
 y = news_df['label']
 
